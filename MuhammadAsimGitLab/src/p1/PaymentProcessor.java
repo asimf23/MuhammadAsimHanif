388db068a6433bc1e3fbc1d85128725ae8934b08
HCC <HCC@DESKTOP-EO0FB0P>
2025-05-13 20:07:04 +0500

    Revert "Processing payment..."
    This reverts commit d74242e08d9c9ad3b4aa0da8812fdc9dab5b9a1f.

--- MuhammadAsimGitLab/src/p1/PaymentProcessor.java
+++
@@ -1,9 +0,0 @@
-package p1;
-
-public class PaymentProcessor {
-    public void processPayment() {
-        System.out.println("Processing payment...");
-        int error = 1 / 0; // Critical bug
-    }
-}
-
