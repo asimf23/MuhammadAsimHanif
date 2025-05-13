5d7ae895a5f0604ca094cf5b44858a810c3ba2d4
HCC <HCC@DESKTOP-EO0FB0P>
2025-05-13 19:49:54 +0500

    Modified login() with local logic

--- MuhammadAsimGitLab/src/p1/UserAuthentication.java
+++ MuhammadAsimGitLab/src/p1/UserAuthentication.java
@@ -2,7 +2,7 @@ package p1;
 
 public class UserAuthentication {
     public void login() {
-        System.out.println("My local login logic.");
+        System.out.println("My local login logic.2");
     }
 }
 
